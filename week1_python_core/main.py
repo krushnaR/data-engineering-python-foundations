b0708123771b6051f251afe8a10ea601a39f2d0f
krushnaR <krushna.ratnaparkhi@gmail.com>
2026-03-20 06:40:07 +0000

    added exception handling to pipeline

--- week1_python_core/main.py
+++ week1_python_core/main.py
@@ -5,36 +5,38 @@ from load import save_data
 from quality_checks import check_row_count, check_negative_claims, check_member_id_null
 import config as c
 
-print("Loading claims data..")
-df = load_data(spark)
+#error handling
+try:
+    print("Loading claims data..")
+    df = load_data(spark)
 
-print("Data Loaded Successfully!")
-print(f"Total number of rows (Raw Data) : {df.count()}")
+    print("Data Loaded Successfully!")
+    print(f"Total number of rows (Raw Data) : {df.count()}")
 
-df,errored_data_dates = clean_claim_dates(df)
-print(f"{errored_data_dates.count()} records found with bad dates/format")
-print(f"Records after removing bad date records : {df.count()}")
+    df,errored_data_dates = clean_claim_dates(df)
+    print(f"{errored_data_dates.count()} records found with bad dates/format")
+    print(f"Records after removing bad date records : {df.count()}")
 
-df,errored_data_neg = remove_negative_claims(df)
-print(f"{errored_data_neg.count()} records found with negative claim amount")
-print(f"Records after removing negative claim records : {df.count()}")
+    df,errored_data_neg = remove_negative_claims(df)
+    print(f"{errored_data_neg.count()} records found with negative claim amount")
+    print(f"Records after removing negative claim records : {df.count()}")
 
-agg_claims = aggregate_claims(df)
-high_cost_members = get_high_cost_members(agg_claims)
+    agg_claims = aggregate_claims(df)
+    high_cost_members = get_high_cost_members(agg_claims)
 
-print("running quality checks on final dataset before loading..")
-c_row_count = check_row_count(high_cost_members)
-c_negative_claims = check_negative_claims(high_cost_members)
-c_member_id_null = check_member_id_null(high_cost_members)
+    print("running quality checks on final dataset before loading..")
+    c_row_count = check_row_count(high_cost_members)
+    c_negative_claims = check_negative_claims(high_cost_members)
+    c_member_id_null = check_member_id_null(high_cost_members)
 
-#Load the data only if all the checks passed.
-if c_row_count and c_negative_claims and c_member_id_null:
-    print("Loading high cost members")
-    save_data(high_cost_members,c.HIGH_COST_TABLE)
-else:
-    print("Quality check failed. Data not loaded.")
+    #Load the data only if all the checks passed.
+    if c_row_count and c_negative_claims and c_member_id_null:
+        print("Loading high cost members")
+        save_data(high_cost_members,c.HIGH_COST_TABLE)
+    else:
+        print("Quality check failed. Data not loaded.")
 
-#agg_claims.display(10)
-high_cost_members.display(10)
-#errored_data_dates.display(10)
-#errored_data_neg.display(10)
+except Exception as e:
+    print(f"Pipeline failed {str(e)}")
+    
+
