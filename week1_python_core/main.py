537af19de55a76f37c5d924161725c1945461a3d
krushnaR <krushna.ratnaparkhi@gmail.com>
2026-03-20 09:34:35 +0000

    Added time tracking for pipeline and saved bad rows

--- week1_python_core/main.py
+++ week1_python_core/main.py
@@ -5,6 +5,9 @@ from load import save_data
 from quality_checks import check_row_count, check_negative_claims, check_member_id_null
 import config as c
 from app_logging import log_info, log_error
+import time
+
+start_time = time.time()
 
 #error handling
 try:
@@ -21,6 +24,9 @@ try:
     df,errored_data_neg = remove_negative_claims(df)
     log_error(f"{errored_data_neg.count()} records found with negative claim amount")
     log_info(f"Records after removing negative claim records : {df.count()}")
+
+    save_data(errored_data_dates,"errored_data_dates")
+    save_data(errored_data_neg,"errored_data_neg")
 
     agg_claims = aggregate_claims(df)
     high_cost_members = get_high_cost_members(agg_claims)
@@ -39,5 +45,7 @@ try:
 
 except Exception as e:
     log_error(f"Pipeline failed {str(e)}")
-    
+
+end_time = time.time()
+log_info(f"Total time taken by pipeline : {end_time - start_time} seconds")    
 
